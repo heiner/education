ac0596fa85560d6fd0ad053ec121ac5532345e9e
Heiner <heiner@inflection.ai>
2023-10-21 13:56:56 +0200

    Add more fancy transitions.

--- www/game.ts
+++ www/game.ts
@@ -1,6 +1,24 @@
 
-function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
+let changes = 0;
+let keyDowns = 0;
+
+// Standard Normal variate using Box-Muller transform.
+function gaussianRandom(mean = 0, stdev = 1) {
+    const u = 1 - Math.random();
+    const v = Math.random();
+    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
+}
+
+function setState(state: string) {
+    const letter = document.getElementById("letter") as HTMLDivElement;
+    const current = letter.innerHTML.trim();
+    if (state === current) {
+        return false;
+    }
+    letter.innerHTML = state;
+    return true;
 }
 
 function toggleFullScreen() {
@@ -14,12 +32,24 @@ function toggleFullScreen() {
 function keyDown(event: KeyboardEvent) {
     let key = event.key;
     if (key.length === 1) {
+        ++keyDowns;
+        const alternate = document.getElementById("alternate") as HTMLInputElement;
+        const size = document.getElementById("size") as HTMLInputElement;
         const letter = document.getElementById("letter") as HTMLDivElement;
-        const random = document.getElementById("random") as HTMLInputElement;
-        if (!random.checked || getRandomInt(2) === 1) {
+        if (!alternate.checked || changes % 2 === 1) {
             key = key.toUpperCase();
         }
-        letter.innerHTML = key;
+        if (setState(key)) {
+            ++changes;
+            letter.style.fontStyle = changes % 5 == 0 ? "italic" : "normal";
+        }
+        console.log(`spin${keyDowns % 2} 0.2s linear 1`);
+        if (size.checked) {
+            letter.style.fontSize = `${gaussianRandom(500, 100)}px`;
+        }
+        letter.style.color =
+            `rgb(${100 * Math.random()}%, ${100 * Math.random()}%, ${100 * Math.random()}%)`;
+        letter.style.animation = `spin${keyDowns % 2} 0.2s linear 1`;
     } else if (key == "Enter") {
         toggleFullScreen();
     }
